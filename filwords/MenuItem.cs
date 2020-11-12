46fc08e650255cd9b950477f56ed5fc562d80d17
Амир <far20022002213@gmail.com>
2020-11-12 23:15:24 +0500

    Menu is ready

---
+++ filwords/MenuItem.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace filwords
+{
+    public class MenuItem
+    {
+        public string name;
+
+        public MenuItem(string n)
+        {
+            name = n;
+            Console.WriteLine(name);
+        }
+    }
+}
--- filwords/Program.cs
+++ filwords/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace filwords
+﻿using System;
+using System.Collections.Generic;
+
+namespace filwords
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            var menuItems = new List<string>
+            {
+                "Новая игра",
+                "Продолжить",
+                "Рейтинг",
+                "Выход"
+            };
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                var selectedMenuItem = Print.Menu(menuItems);
+
+                if (selectedMenuItem == "Новая игра")
+                {
+                    
+                }
+                else if (selectedMenuItem == "Продолжить")
+                {
+                    
+                }
+                else if (selectedMenuItem == "Рейтинг")
+                {
+                    
+                }
+                else if (selectedMenuItem == "Выход")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
     }
-  }
 }
